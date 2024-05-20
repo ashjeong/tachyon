3c2a04415540de2ff2ec17a65b667e0b9fdcbc61
Ryan Kim <chokobole33@gmail.com>
2024-05-20 17:44:49 +0900

    refac(math): remove unused methods from small prime fields and goldilocks

--- tachyon/math/finite_fields/small_prime_field_mont.h
+++ tachyon/math/finite_fields/small_prime_field_mont.h
@@ -17,7 +17,6 @@
 #include "tachyon/base/strings/string_number_conversions.h"
 #include "tachyon/base/strings/string_util.h"
 #include "tachyon/build/build_config.h"
-#include "tachyon/math/base/egcd.h"
 #include "tachyon/math/finite_fields/prime_field_base.h"
 
 namespace tachyon::math {
@@ -120,13 +119,6 @@ class PrimeField<_Config, std::enable_if_t<!_Config::kIsSpecialPrime &&
   // TODO(chokobole): Support bigendian.
   constexpr BigInt<N> ToBigInt() const {
     return BigInt<N>(Config::FromMontgomery(value_));
-  }
-
-  constexpr operator uint32_t() const { return value_; }
-
-  constexpr uint32_t operator[](size_t i) const {
-    DCHECK_EQ(i, size_t{0});
-    return value_;
   }
 
   constexpr bool operator==(PrimeField other) const {
