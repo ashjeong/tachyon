6a954e819242d64a6af0480f0c8182b4983fae95
ashjeong <ashjeong@umich.edu>
2024-09-10 11:14:00 +0900

    tgg

--- tachyon/crypto/hashes/sponge/poseidon2/poseidon2_plonky3_internal_matrix.h
+++ tachyon/crypto/hashes/sponge/poseidon2/poseidon2_plonky3_internal_matrix.h
@@ -29,7 +29,7 @@ class Poseidon2Plonky3InternalMatrix {
     if constexpr (PrimeField::Config::kUseMontgomery) {
       static_assert(PrimeField::Config::kModulusBits <= 32);
       for (F& f : v) {
-        f *= F::RawOne();
+        f *= F::Broadcast(PrimeField::One());
       }
     }
   }
